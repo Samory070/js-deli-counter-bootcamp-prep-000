d4f86dbd80ff729140145fd19c7af08f42098471
samory harris <samory070@gmail.com>
2018-02-20 19:06:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine(line) {
     return 'The line is currently empty.'
   }
   const numsAndNames = []
-  for(let i = 0; i < line.length: i++) {
+  for(let i = 0; i < line.length; i++) {
     numsAndNames.push(`${i + 1}. ${line[i]}`)
   }
   return `The line is currently: ${numsAndNames.join(',')}`
